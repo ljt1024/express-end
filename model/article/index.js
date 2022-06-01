9d41ce60ae1804ec9946b1bd48e32cca296e1654
ljt1024 <1652009833@qq.com>
2022-06-01 10:23:27 +0800

    fix:修改文章表

--- model/article/index.js
+++ model/article/index.js
@@ -1,16 +1,35 @@
 // 建立文章表
 const ArticleSchema = {
+    // 文章标题
     title: {
         type: String,
         unique: true
     },
+    // 文章描述
     abstract: {
         type: String
     },
+    // 文章内容
     content: {
         type: String
     },
+    // 文章创建时间
     creatTime: {
+        type: String
+    },
+    // 作者
+    author: {
+        type: String
+    },
+    // 评论数
+    comments: {
+        type: String
+    },
+    // 点赞数
+    thumbs: {
+        type: String
+    },
+    category: {
         type: String
     }
 }
--- model/index.js
+++ model/index.js
@@ -3,10 +3,15 @@ const mongoose = require('mongoose')
 const UserOption = require('./login')
 const ArticleOption = require('./article')
 // 连接数据库，自动新建 ExpressAuth 库
-mongoose.connect('mongodb://localhost:27017/ExpressAuth', {
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
+try {
+    mongoose.connect('mongodb://localhost:27017/ExpressAuth', {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+} catch (e) {
+
+}
+
 
 // 建立用户表
 const UserSchema = new mongoose.Schema(UserOption)
