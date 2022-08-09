c66063e136f7ac1da5d883158f2e7399b97127c0
ljt1024 <1652009833@qq.com>
2022-08-09 23:11:13 +0800

    fix:完善聊天

--- server.js
+++ server.js
@@ -4,24 +4,38 @@ const router = require('./router/index')
 const path = require('path');
 const cors = require('cors')
 const expressWs = require('express-ws')
-
+const { Chart } = require('./model/index')
 // 创建服务器应用程序
 const app = express()
 expressWs(app)
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
-
 app.use(router)
-app.ws("/socketTest", function (ws, req) {
-  ws.send("你连接成功了");
-  ws.on("message", function (msg) {
-    console.log(msg)
 
-    ws.send("这是第二次发送信息");
+// ws 集合
+let connections = []
+app.ws("/socketTest", function(ws, req) {
+  ws.on("message", async function (msg) {
+    //  这里用户刚进来的时候push
+    connections.push(ws)
+    await Chart.create({...JSON.parse(msg), createTime: new Date().toString()})
+    allSend(JSON.stringify({code:200 , msg: '服务器接收消息成功'}))
+    // ws.send(), (err)=> {
+    //   console.log(err)
+    // })
+
   });
+
 });
 
+// 群发
+function allSend(msg) {
+  console.log(connections,'集合');
+  connections.map(item => {
+    item.send(msg)
+  })
+}
 app.use((err, req, res, next)=> {
   console.error('Error:', err);
   res.status(500).send('Service Error');
